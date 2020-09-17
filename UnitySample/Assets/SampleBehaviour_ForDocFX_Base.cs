1389550b913e88a7dadba72843309131f4513312
KorStrix <Korstrix@gmail.com>
2020-09-17 12:46:57 +0900

    Filter 커밋

--- UnitySample/Assets/SampleBehaviour_ForDocFX_Base.cs
+++ UnitySample/Assets/SampleBehaviour_ForDocFX_Base.cs
@@ -2,32 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 이 코드는 샘플입니다. <see cref="SampleBehaviour_ForDocFX"/>
-/// </summary>
-public class SampleBehaviour_ForDocFX_Base : MonoBehaviour
+
+namespace DocFXTest
 {
     /// <summary>
-    /// 이 함수는 Start 함수입니다.
+    /// 이 코드는 샘플입니다. <see cref="SampleBehaviour_ForDocFX"/>
     /// </summary>
-    void Start()
+    public class SampleBehaviour_ForDocFX_Base : MonoBehaviour
     {
-        
-    }
+        /// <summary>
+        /// 이 함수는 Start 함수입니다.
+        /// </summary>
+        void Start()
+        {
 
-    /// <summary>
-    /// 이 함수는 public 함수입니다.
-    /// </summary>
-    public void Public_Function()
-    {
+        }
 
-    }
+        /// <summary>
+        /// 이 함수는 public 함수입니다.
+        /// </summary>
+        public void Public_Function()
+        {
 
-    /// <summary>
-    /// 이 함수는 virtual 함수입니다.
-    /// </summary>
-    public virtual void Virtual_Function()
-    {
+        }
 
+        /// <summary>
+        /// 이 함수는 virtual 함수입니다.
+        /// </summary>
+        public virtual void Virtual_Function()
+        {
+
+        }
     }
 }
